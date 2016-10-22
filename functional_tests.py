b51fd53e32c081c410d3cbca07ebb721c35fce0e
Thomas Utterback <timba@noisycrow.com>
2016-10-22 13:41:13 -0700

    Functional test now checks if we can inut a to-do list item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -14,19 +15,23 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
         # page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
         # invitation to enter a to-do list item
-
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        sefl.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
         # "Buy peacock feathers" into box
-
-        # hit Enter 
-
+        inputbox.send_keys('Buy peacock feathers')
+        # hit Enter
+        inputbox.send_keys(Keys.ENTER)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
         # add "Use peacock feathers to make a fly" (ie, for fishing)
-
+        self.fail('Finish the test!')
         # site creates unique URL for list
 
         # visit URL, site is there
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
-
